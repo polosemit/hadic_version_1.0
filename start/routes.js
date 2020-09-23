2f6ec6161180b832ff5204e33ed88190bf00157a
Firdaus bin Hayali <firdaus.hayali@serunai.com>
2020-09-23 11:46:01 +0800

    first commit

---
+++ start/routes.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.on('/').render('welcome')
+
+Route.post('/api/hadic/call', 'SmartcontractController.MethodCall')
+
+Route.post('/api/hadic/send', 'SmartcontractController.MethodSend')
+
+Route.any('*', ({response}) => {
+    response.json({error: "Invalid URL, Please check url!!!"})
+})
